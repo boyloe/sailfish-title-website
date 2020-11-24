fb395f64ed343e3094932b290817be91f68b62b2
boyloe <boyloe@gmail.com>
2020-11-23 17:33:50 -0700

    Add nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Logo from './assets/Logo/Black on Transparent.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jumbotron fluid>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">
+            <Image src={Logo} alt='sailfish logo' fluid />            
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" />
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#quote">Quote</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar>
+        <Container>
+          <Image src={Logo} alt='sailfish logo' fluid/>
+        </Container>
+        <Container>
+      
+        </Container>
+
+
+      </Jumbotron>
     </div>
   );
 }
